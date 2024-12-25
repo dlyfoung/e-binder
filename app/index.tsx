45eaa389386ab95ed01048e19549d74871a1a6bd
Toujon Lyfoung <dlyfoung@gmail.com>
2024-12-25 16:41:40 -0600

    add search bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import { Heading } from "@/components/ui/heading";
+import { SearchIcon } from "@/components/ui/icon";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import React from "react";
 import { Text, View } from "react-native";
 
@@ -11,9 +13,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Input>
+        <InputField placeholder="Search..." />
+        <InputSlot>
+          <InputIcon as={SearchIcon}/>
+        </InputSlot>
+      </Input>
       <Heading>I am a Heading</Heading>
-      <Text>Hello World!! This is so cool. I know who you are. You are ponia
-      . ah ah. awsome
+      <Text>Hello World!! This is so cool. I know who you are.I am someone you may not know.You'll not know me now, and you won't know me in 10,000 years.BLAH BLAH BLAH TODAY WAS TERRIBLE AND WAS SUPER BORING.THE MOST BORING DAY IN THE WHOLE ENTIRE UNIVERSE
       </Text>
     </View>
   );
