25368d348a1f65ca0febd3d1daaa51a20d47d00d
Toujon Lyfoung <dlyfoung@gmail.com>
2024-12-28 15:12:40 -0600

    linting

--- app/index.tsx
+++ app/index.tsx
@@ -16,9 +16,9 @@ export default function Index() {
       <Heading>I am a Heading</Heading>
       <Text>
         Hello World!! This is so cool. I know who you are.I am someone you may
-        not know.You'll not know me now, and you won't know me in 10,000
-        years.BLAH BLAH BLAH TODAY WAS TERRIBLE AND WAS SUPER BORING.THE MOST
-        BORING DAY IN THE WHOLE ENTIRE UNIVERSE
+        not know.You&apos;ll not know me now, and you won&apos;t know me in
+        10,000 10,000 years.BLAH BLAH BLAH TODAY WAS TERRIBLE AND WAS SUPER
+        BORING.THE BORING DAY IN THE WHOLE ENTIRE UNIVERSE
       </Text>
     </View>
   );
