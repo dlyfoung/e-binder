d026f6e4bdd38606ae8c7ec3ed876da37db5ed95
Toujon Lyfoung <dlyfoung@gmail.com>
2025-01-13 16:23:49 -0600

    Add menu items

--- app/menu/SideMenu.tsx
+++ app/menu/SideMenu.tsx
@@ -8,9 +8,24 @@ import {
   DrawerHeader,
 } from "@/components/ui/drawer";
 import { Heading } from "@/components/ui/heading";
+import { Icon, RemoveIcon, RepeatIcon } from "@/components/ui/icon";
+import { Pressable } from "@/components/ui/pressable";
 import { Text } from "@/components/ui/text";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  menuIcon: {
+    marginRight: 5,
+  },
+  menuItem: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: 2,
+    marginTop: 2,
+  },
+});
 
 export default function SideMenu({ onClose, show }: SideMenuProps) {
   const { t } = useTranslation();
@@ -20,10 +35,17 @@ export default function SideMenu({ onClose, show }: SideMenuProps) {
       <DrawerBackdrop />
       <DrawerContent>
         <DrawerHeader>
-          <Heading>{t("menu")}</Heading>
+          <Heading>{t("settings")}</Heading>
         </DrawerHeader>
         <DrawerBody>
-          <Text>TODO</Text>
+          <Pressable style={styles.menuItem}>
+            <Icon as={RepeatIcon} style={styles.menuIcon} />
+            <Text>{t("reload-document")}</Text>
+          </Pressable>
+          <Pressable style={styles.menuItem}>
+            <Icon as={RemoveIcon} style={styles.menuIcon} />
+            <Text>{t("wipe-data")}</Text>
+          </Pressable>
         </DrawerBody>
         <DrawerFooter>
           <Button onPress={onClose} style={{ flex: 1 }}>
